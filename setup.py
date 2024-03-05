45157379caeabe2380fd14a3bdffbe40d3f43de9
delveccj <delveccj@canisius.edu>
2024-03-05 05:44:05 -0500

    Changes to setup.py and start-sonar.sh

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ install_reqs = parse_requirements('requirements.txt')
 setup(
     name='module7_static_code_analysis',
     version='1.0.0',
-    author_email='baumgar2@canisius.edu',
-    author='Kyle Baumgardner',
+    author_email='delveccj@canisius.edu',
+    author='Justin Del Vecchio',
     packages=['cyb600_module7'],
     tests=['tests'],
     license='Apache License Version 2.0',
--- start-sonar.sh
+++ start-sonar.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 mkdir -p ${PWD}/sonar_persist/sonarqube_data
 mkdir -p ${PWD}/sonar_persist/sonarqube_extensions
 mkdir -p ${PWD}/sonar_persist/sonarqube_logs
+
 docker run --name=sonarqube -d -p 9000:9000 \
--v ${PWD}/sonar_persist/sonarqube_data:/opt/sonarqube/data \
--v ${PWD}/sonar_persist/sonarqube_extensions:/opt/sonarqube/extensions \
--v ${PWD}/sonar_persist/sonarqube_logs:/opt/sonarqube/logs \
-sonarqube
+  -v ${PWD}/sonar_persist/sonarqube_data:/opt/sonarqube/data \
+  -v ${PWD}/sonar_persist/sonarqube_extensions:/opt/sonarqube/extensions \
+  -v ${PWD}/sonar_persist/sonarqube_logs:/opt/sonarqube/logs \
+  sonarqube:9.9.0-community
